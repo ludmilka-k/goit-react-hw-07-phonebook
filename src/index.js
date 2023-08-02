2a01586ecf4e6f131a2785f00260dcf1d1dd20dc
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-02 19:01:22 +0300

    Remove persist gate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
 import { ThemeProvider } from '@emotion/react'
 import { App } from 'components/App';
 import {store} from './redux';
