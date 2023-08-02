1425ae686616e4b42fc7c6d46d9b9e6463303c2b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-03 00:26:22 +0300

    Update selectors.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
@@ -5,6 +7,13 @@ import {Section} from './Section'
 
 
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const dispatch = useDispatch;
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch]);
 
   return (
     <>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,17 @@
+import {createSelector} from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts;
 export const getFilter = state => state.filter;
+export const getIsLoading = state => state.isLoading;
+export const getError = state => state.error;
+
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter], (contacts, filter) => {
+    if(!filter) {
+      return contacts;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
