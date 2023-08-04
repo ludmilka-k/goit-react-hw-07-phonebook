05393f99769f0a16fd9227d5afa5cc9bb78fbf64
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-04 20:34:31 +0300

    Fix contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -31,8 +31,7 @@ const contactsSlice = createSlice({
       .addCase(addContactThunk.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        // state.items = state.items.concat([action.payload]);
-        state.items.unshift(action.payload);
+        state.items = state.items.concat([action.payload]);
       })
       .addCase(addContactThunk.rejected, (state, action) => {
         state.isLoading = false;
@@ -45,12 +44,12 @@ const contactsSlice = createSlice({
       .addCase(deleteContactThunk.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        // state.items = state.items.filter(contact => contact.id !== action.payload);
+        state.items = state.items.filter(contact => contact.id !== action.payload.id);
 
-        const deletedContactIndex = state.items.findIndex(
-          contact => contact.id === action.payload.id
-        );
-        state.items.splice(deletedContactIndex, 1)
+        // const deletedContactIndex = state.items.findIndex(
+        //   contact => contact.id === action.payload.id
+        // );
+        // state.items.splice(deletedContactIndex, 1)
       })
       .addCase(deleteContactThunk.rejected, (state, action) => {
         state.isLoading = false;
@@ -61,46 +60,4 @@ const contactsSlice = createSlice({
 export const contactsReducer = contactsSlice.reducer;
 
 
-
-
-
-// const sharedActions = type => isAnyOf(
-//   fetchContactsThunk[type],
-//   addContactThunk[type],
-//   deleteContactThunk[type]
-// );
-//
-// const contactsSlice = createSlice({
-//   name:'contacts',
-//   initialState,
-//   extraReducers: builder =>
-// // Uncaught Error: addCase cannot be called with two reducers for the same action type
-// // Solution: https://github.com/reduxjs/redux-toolkit/issues/429
-//     builder
-//       .addCase(fetchContactsThunk.fulfilled, (state, action) => {
-//         state.contacts = action.payload;
-//       })
-//       .addCase(addContactThunk.fulfilled, (state, action) => {
-//         state.contacts = state.contacts.push(action.payload);
-//       })
-//       .addCase(deleteContactThunk.fulfilled, (state, action) => {
-//         state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
-//         //or second way
-//         // const deletedContactIndex = state.contacts.findIndex(
-//         //   contact => contact.id === action.payload
-//         // );
-//         // state.contacts.splice(deletedContactIndex, 1)
-//       })
-//       .addMatcher(sharedActions('pending'), state => {
-//         state.isLoading = true;
-//       })
-//       .addMatcher(sharedActions('rejected'), (state, action) => {
-//         state.isLoading = false;
-//         state.error = action.payload;
-//       })
-//       .addMatcher(sharedActions('fulfilled'), (state) => {
-//         state.isLoading = false;
-//         state.error = null;
-//       })
-// });
 
